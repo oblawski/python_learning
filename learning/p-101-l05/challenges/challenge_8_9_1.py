a8f1e72ed20f0c0f41834f44b21134dc46c707ae
Bartosz Oblawski <bartosz.oblawski@icloud.com>
2023-03-07 11:20:57 +0100

    l05 challenge fix bugs

--- learning/p-101-l05/challenges/challenge_8_9_1.py
+++ learning/p-101-l05/challenges/challenge_8_9_1.py
@@ -1,13 +1,13 @@
 from random import random
 
-region_1 = 87
-region_2 = 65
-region_3 = 17
+region_1 = 0.87
+region_2 = 0.65
+region_3 = 0.17
 
 win_overall = 0
 lose_overall = 0
-
-for i in range(10_000):
+num_trials = 10_000
+for i in range(num_trials):
     win = 0
     lose = 0
     if random() < region_1:
@@ -27,7 +27,8 @@ for i in range(10_000):
         win_overall += 1
     else:
         lose_overall += 1
-ratio = lose_overall / win_overall
+
+ratio = win_overall / num_trials
 print(f"Candidate A has {ratio:.1%} chance to win")
 print(f"Candidate B has {1 - ratio:.1%} chance to win")
 
